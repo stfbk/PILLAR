f5d2f4489b847dade2bc3ed4cfd67c9b252ce69f
Andrea Bissoli <abisso2003@gmail.com>
2024-09-05 10:30:29 +0200

    Updated Tool Architecture

--- tabs/sidebar.py
+++ tabs/sidebar.py
@@ -172,7 +172,7 @@ def sidebar():
 
         **P**rivacy risk **I**dentification with **L**INDDUN and **L**LM **A**nalysis **R**eport
         
-        You can download the Tool Architecture diagram [here](/app/static/ToolArchitecture.png).
+        You can download the Tool Architecture diagram [here](/app/static/Tool-Architecture.png).
         """
         )
         st.markdown(
